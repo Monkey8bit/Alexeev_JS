5639cfd35aafa1da7ce660924fa216c38e08e72a
Monkey8bit <de_construct@mail.ru>
2021-12-08 00:37:13 +0400

    lesson_1

---
+++ lesson_1/script.js
@@ -0,0 +1,16 @@
+
+//task 1:
+
+var celsius = Math.floor(Math.random() * 100);
+var fahrenheit = (9 / 5) * celsius + 32;
+
+alert(`${celsius} degrees by celsius.\n${Number(fahrenheit)} degrees by fahrenheit.`);
+
+//task 2:
+
+var name = "Василий";
+var admin = name;
+alert(admin);
+
+//task 3:
+
